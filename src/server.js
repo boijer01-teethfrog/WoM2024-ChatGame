47a03df8949d98b077c2207fa7bb1661092a4586
Simon Boijer <boijersi@arcada.fi>
2024-09-30 14:21:01 +0300

    please work

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
