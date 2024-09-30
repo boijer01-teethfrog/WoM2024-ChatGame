ab3cf9e9a89f8147dc06e69232cc8b57c79c79bb
Simon Boijer <boijersi@arcada.fi>
2024-09-30 14:13:26 +0300

    Troubleshooting deployment

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
