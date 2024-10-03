e141b854288d7e420442ea7ed5c9cbafeb7e8231
Alexander Kasianov <117091619+teethfrog@users.noreply.github.com>
2024-10-03 14:29:18 +0300

    fixing select
    Colors work, join room works

--- public/scripts/select.js
+++ public/scripts/select.js
@@ -3,5 +3,46 @@ import { getUsername, getHex } from "./config.js";
 
 document.getElementById('greetingText').textContent = `Welcome ${getUsername()}!`
 document.getElementById('account-name').textContent = `Name: ${getUsername()}`;
-document.getElementById('color-box').style.backgroundColor = getHex();
 document.getElementById('color-picker').value = getHex();
+
+
+let debounceTimer;
+
+document.getElementById('color-picker').addEventListener('input', (event) => {
+    const newColor = event.target.value;
+
+    clearTimeout(debounceTimer);
+
+    debounceTimer = setTimeout(() => {
+        (async () => {
+            try {
+                const response = await fetch('/user/updateColor', {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: getUsername(),
+                        hex: newColor,
+                    }),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const result = await response.json();
+                console.log('Color update result:', result);
+                localStorage.setItem('hex', newColor);
+            } catch (error) {
+                console.error('Error updating color:', error);
+            }
+        })();
+    }, 500);
+});
+
+document.getElementById('join-room-button').addEventListener('click', (event) => {
+    const roomId = document.getElementById('room-code').value;
+    console.log(roomId);
+    window.location.href = `/room/`;
+});
